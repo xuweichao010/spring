c8252cac2925b7c496de18f2910d3964f7c52fe2
cc <wcxu@incarcloud.com>
2020-12-05 21:14:41 +0800

    注释代码

--- demo/src/main/java/com/xwc/demo/SpringDemo.java
+++ demo/src/main/java/com/xwc/demo/SpringDemo.java
@@ -3,7 +3,6 @@ package com.xwc.demo;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 
 /**
  * 创建人：徐卫超 CC
